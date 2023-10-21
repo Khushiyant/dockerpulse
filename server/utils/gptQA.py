7ffa763c97c68bb88f00a77b03842b48ccc59daa
Vidhu Mathur <vidhumathur2002@gmail.com>
2023-10-22 03:47:41 +0530

    chore(add): Class for giving sol for anamoly(gpt)

---
+++ server/utils/gptQA.py
@@ -0,0 +1,44 @@
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from duckduckgo_search import DDGS
+from dotenv import load_dotenv
+import re
+
+# Load .env file
+load_dotenv()
+
+class GPTQA:
+    def __init__(self, api_key):
+        openai.api_key = api_key
+        self.model_engine = "gpt-3.5-turbo"
+        self.prompt = "I have a problem with my Docker logs. Can you help me troubleshoot?"
+        self.temperature = 0.5
+        self.max_tokens = 2048
+
+    def search(query):
+        with DDGS() as ddgs:
+            for r in ddgs.text(query):
+                return r
+    
+    def generate_solution(self, logs):
+
+        tools = [
+                Tool(
+                name="search",
+                func=self.search,
+                description="Useful when you need to search for something on the internet.",
+            ),]
+        # Clean up the logs to remove any sensitive information
+        logs = re.sub(r"([0-9a-fA-F]{64})", "<HASH>", logs)
+        logs = re.sub(r"([0-9]{1,3}\.){3}[0-9]{1,3}", "<IP_ADDRESS>", logs)
+        
+        # Generate a solution using OpenAI's GPT-3 API
+        prompt = f"{self.prompt}\n\nLogs:\n{logs}\n\nSolution:"
+
+        llm = ChatOpenAI(model_name=self.model_engine,temperature=self.temperature,max_tokens=self.max_tokens)
+        agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_MULTI_FUNCTIONS, verbose=True)
+        answer =  agent.run(prompt)
+        return answer
+        
